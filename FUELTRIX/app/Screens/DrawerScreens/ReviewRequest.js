5b7addd0312e91cf9b24bc0f4ba4e7eba976eb07
Nadil De Silva <161411320+NadilDeSilva05@users.noreply.github.com>
2024-10-10 09:15:28 +0530

    drawer styles added

---
+++ FUELTRIX/app/Screens/DrawerScreens/ReviewRequest.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ReviewRequest() {
+  return (
+    <View>
+      <Text>ReviewRequest</Text>
+    </View>
+  )
+}
